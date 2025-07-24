b7e384d188eb7bb5f57c4af8347c8a53ac8a763d
IcePanorama <thriftedroot@protonmail.com>
2025-07-24 11:02:38 -0400

    Reading file, processing samples.

--- cmp_file.py
+++ cmp_file.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 
 class CMPFile:
@@ -10,7 +11,30 @@ class CMPFile:
 
     def __init__(self, filename: str):
         self._filename: str = filename
+        self._size_bytes: int = os.path.getsize(self._filename)
+        raw_data: bytes
+
         logging.info(f"Creating CMP file from {filename}")
-        self._data: bytes
         with open(self._filename, "rb") as fptr:
-            self._data = fptr.read()
+            raw_data = fptr.read()
+
+        self._process_data(raw_data)
+        print(self._samples)
+
+    def _process_data(self, data: bytes) -> None:
+        self._samples: [int] = []
+        b: int
+        for b in data:
+            # See: "Dialogic ADPCM Algorithm", pg. 3
+            curr: [int] = [(b & 0xFF) >> 4, b & 0xF]
+            curr = [self._sample_to_signed_int(s) for s in curr]
+            self._samples.extend(curr)
+
+    def _sample_to_signed_int(self, sample: int) -> int:
+        """
+            Converts unsigned 4-bit int to a signed one.
+            See: "Dialogic ADPCM Algorithm", pg. 3
+        """
+        out: int = sample & 7  # 3 LSBs = magnitude
+        sign: int = (sample & 8) >> 3  # MSB = sign
+        return out if sign == 0 else -out
