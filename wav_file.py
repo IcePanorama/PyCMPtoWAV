ef79ac07b80ea58d02d62b24eff11d7f50330adc
IcePanorama <thriftedroot@protonmail.com>
2025-08-01 10:54:37 -0400

    Normalizing waveform prior to export.

--- wav_file.py
+++ wav_file.py
@@ -1,6 +1,7 @@
 from cmp.file import CMPFile
 from io import BufferedWriter
 import logging
+from typing import List
 
 
 class WaveformAudioFile:
@@ -8,17 +9,14 @@ class WaveformAudioFile:
         Output is a signed, 24-bit PCM WAV.
         See: https://en.wikipedia.org/wiki/WAV
     """
-    _BYTE_ORDER = "little"
+    _BYTE_ORDER: str = "little"
     _SAMPLE_RATE_HZ: int = 22050  # Harvester's wav files use this sample rate
-    _BYTES_PER_SAMPLE = 2  # Decoded output is 12-bits
+    _BYTES_PER_SAMPLE: int = 4
 
     def __init__(self, cmp: CMPFile, filename: str = ""):
-        self._cmp = cmp
-        self._filename: str
-        if filename:
-            self._filename = filename
-        else:
-            self._filename = self._create_filename(cmp.filename)
+        self._waveform = self._normalize_waveform(cmp.waveform)
+        self._filename: str = filename if filename else self._create_filename(
+            cmp.filename)
 
     def _create_filename(self, cmp_filename: str) -> str:
         ext_idx: int = cmp_filename.find(".")
@@ -27,17 +25,25 @@ class WaveformAudioFile:
         name: str = cmp_filename[:ext_idx + 1] + "wav"
         return name
 
+    def _normalize_waveform(self, waveform: List[int]) -> List[int]:
+        """
+            CMP decoded waveform is a signed 12-bit PCM "linear output sample"
+            waveform however, we need this value to be normalized according to
+            the range specified by `self._BYTES_PER_SAMPLE`.
+        """
+        U12_MAX: int = (2**12) - 1
+        HALF_U12_MAX: int = (U12_MAX + 1) >> 1
+        work: List[float] = [((s + HALF_U12_MAX) / U12_MAX) for s in waveform]
+        work = [max(0.0, min(s, 1.0)) for s in work]
+
+        UW_MAX: int = (2**(self._BYTES_PER_SAMPLE << 3)) - 1
+        HALF_UW_MAX: int = (UW_MAX + 1) >> 1
+        return [int(s * UW_MAX - HALF_UW_MAX) for s in work]
+
     def _write_header(self, fptr: BufferedWriter):
         """
-            See: https://en.wikipedia.org/wiki/WAV#WAV_file_header
+        See: https://en.wikipedia.org/wiki/WAV#WAV_file_header
         """
-        # bits per sample
-        bpsamp: int = self._BYTES_PER_SAMPLE * 8
-        # bits per bloc = nChannels * bits per sample / 8
-        bpb: int = (1 * bpsamp) >> 3
-        # bytes per sec = freq * bytes per bloc
-        bpsec: int = self._SAMPLE_RATE_HZ * bpb
-
         """ Master RIFF chunk """
         fptr.write(b"RIFF")
         fptr.seek(4, 1)  # skipping file size for now
@@ -45,13 +51,18 @@ class WaveformAudioFile:
 
         """ Fmt chunk """
         fptr.write(b"fmt ")  # added space is intentional
-        # chunk bloc size
-        fptr.write(0x10.to_bytes(4, self._BYTE_ORDER))
-        # PCM integer format
-        fptr.write(0x1.to_bytes(2, self._BYTE_ORDER))
+        fptr.write(0x10.to_bytes(4, self._BYTE_ORDER))  # chunk bloc size
+        fptr.write(0x1.to_bytes(2, self._BYTE_ORDER))  # PCM integer format
         # Mono audio (1 channel)
         fptr.write(0x1.to_bytes(2, self._BYTE_ORDER))
         fptr.write(self._SAMPLE_RATE_HZ.to_bytes(4, self._BYTE_ORDER))
+
+        bpsamp: int = (self._BYTES_PER_SAMPLE << 3)  # bits per sample
+        # bits per bloc = nChannels * bits per sample / 8
+        bpb: int = (1 * bpsamp) >> 3
+        # bytes per sec = freq * bytes per bloc
+        bpsec: int = self._SAMPLE_RATE_HZ * bpb
+
         fptr.write(bpsec.to_bytes(4, self._BYTE_ORDER))
         fptr.write(bpb.to_bytes(2, self._BYTE_ORDER))
         fptr.write((bpsamp).to_bytes(2, self._BYTE_ORDER))
@@ -62,16 +73,13 @@ class WaveformAudioFile:
 
     def _write_size_data(self, fptr: BufferedWriter):
         """
-            Need to now export the data we skipped in `_write_header`.
+        Need to now export the data we skipped in `_write_header`.
         """
-        """ Update file size. """
         file_size: int = fptr.tell()
-        logging.debug(f"File size: {file_size} ({file_size:X})")
-        fptr.seek(4, 0)  # file size
+        logging.debug(f"Output file size: {file_size} ({file_size:X})")
+        fptr.seek(4, 0)
         rem: int = file_size - 8
-        logging.debug("Remaining file size after RIFF header: " +
-                      f"{file_size - 8}")
-        fptr.write((file_size - 8) .to_bytes(4, self._BYTE_ORDER))
+        fptr.write((file_size - 8).to_bytes(4, self._BYTE_ORDER))
 
         """ Update sampled data size. """
         fptr.seek(0x28, 0)
@@ -83,8 +91,8 @@ class WaveformAudioFile:
         with open(self._filename, "wb", buffering=0) as fptr:
             self._write_header(fptr)
             sample: int
-            for sample in self._cmp.waveform:
-                fptr.write(sample.to_bytes(
-                    self._BYTES_PER_SAMPLE, self._BYTE_ORDER, signed=True))
+            for sample in self._waveform:
+                fptr.write(sample.to_bytes(self._BYTES_PER_SAMPLE,
+                           self._BYTE_ORDER, signed=True))
 
             self._write_size_data(fptr)
